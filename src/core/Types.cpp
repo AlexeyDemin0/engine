c881140e2dff18b7511bdbbebd26747222987a4a
Alexey Demin <AlexeyDeminA@gmail.com>
2025-06-15 02:58:01 +0300

    Add Types file

---
+++ src/core/Types.cpp
@@ -0,0 +1,10 @@
+/**
+ * SPDX-License-Identifier: MIT
+ *
+ * @file Types.cpp
+ * @brief All implementation contains in header file Types.h
+ *
+ * @author Alexey Demin (AlexeyDeminA@gmail.com)
+ */
+
+#include "core/Types.h"
---
+++ src/core/Types.h
@@ -0,0 +1,28 @@
+/**
+ * SPDX-License-Identifier: MIT
+ *
+ * @file Types.h
+ * @brief Contains definitions for base types
+ *
+ * @author Alexey Demin (AlexeyDeminA@gmail.com)
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace Engine::Core
+{
+
+using i8 = int8_t;
+using u8 = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+} // namespace Engine::Core
